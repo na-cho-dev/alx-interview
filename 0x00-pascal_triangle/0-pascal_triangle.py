bb59f5dcfc95474ddd81c2a2bbb4a605ff71dd91
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-06 00:47:37 +0100

    0x00. Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,6 +10,11 @@ Pascal’s triangle of n:
 
 
 def pascal_triangle(n):
+    """
+    Returns an empty list if n <= 0
+    else list of lists of integers representing
+    the Pascal’s triangle of n
+    """
     if n <= 0:
         return []
 
