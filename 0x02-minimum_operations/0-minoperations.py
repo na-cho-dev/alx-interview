479622345a3c29f430fd3e2a43ddd0fe88d7daf9
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-17 17:05:19 +0100

    Task 0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+n a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+
+    Prototype: def minOperations(n)
+    Returns an integer
+    If n is impossible to achieve, return 0
+Example:
+
+n = 9
+
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+
+Number of operations: 6
+"""
+
+def minOperations(n: int) -> int:
+    div_num = 2
+    prime_factors = []
+    condition = True
+    num_of_op = 0
+
+    while condition:
+        if ((n % div_num == 0) and (n != 1)):
+            num_of_op += div_num
+            n //= div_num
+        elif ((n % div_num != 0) and (n != 1)):
+            div_num += 1
+        else:
+            condition = False
+
+    return num_of_op
+    
+#     print(prime_factors)
+
+# if __name__ == "__main__":
+#     print(minOperations(9))
+
