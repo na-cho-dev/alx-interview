cf7b049f1b17752a5eda8393449be0125135f4b3
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-12 18:08:25 +0100

    Task 0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,56 @@
 #!/usr/bin/node
+const request = require('request');
+
+const requestPromise = (url) => {
+  const promise = new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else if (res && res.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(
+          new Error(
+            `Failed to fetch data. Status Code: ${res && res.statusCode}`
+          )
+        );
+      }
+    });
+  });
+
+  return promise;
+};
+
+const requestsAPI = async (url) => {
+  try {
+    const data = await requestPromise(url);
+    const body = data.characters ? data.characters : data.name;
+    // console.log(body);
+
+    if (body instanceof Array) {
+      for (const people of body) {
+        await requestsAPI(people);
+        console.log(people);
+      }
+    } else {
+      console.log(body);
+    }
+  } catch (err) {
+    console.error('Error fetching data:', err);
+  }
+};
+
+const printsSWChar = () => {
+  const args = process.argv;
+
+  if (args.length !== 3) {
+    console.log('Script requires one argument');
+    return;
+  }
+
+  const movieID = args[2];
+
+  requestsAPI(`https://swapi-api.alx-tools.com/api/films/${movieID}`);
+};
+
+printsSWChar();
